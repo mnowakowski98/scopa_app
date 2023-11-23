aefe646f60268806b861c7e0868eb84d12499dfd
Matthew Nowakowski <nowakowskim98@gmail.com>
2023-11-22 21:22:13 -0500

    Wrapped GamePage pumps with MaterialApp

--- test/game_test.dart
+++ test/game_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:scopa_app/game.dart';
 import 'package:scopa_lib/scopa_lib.dart';
@@ -5,17 +6,19 @@ import 'package:scopa_lib/tabletop_lib.dart';
 
 void main() {
   testWidgets('Shows a list of players', (tester) async {
-    await tester.pumpWidget(GamePage(
-        game: Game({
-      Team.players([Player('Test 1')]),
-      Team.players([Player('Test 2')]),
-    })));
+    await tester.pumpWidget(MaterialApp(
+      home: GamePage(
+          game: Game({
+        Team.players([Player('Test 1')]),
+        Team.players([Player('Test 2')]),
+      })),
+    ));
 
     expect(find.text('Test 1'), findsOneWidget);
     expect(find.text('Test 2'), findsOneWidget);
   });
   testWidgets('Shows the table hand cards', (tester) async {
-    await tester.pumpWidget(GamePage(game: Game({})));
+    await tester.pumpWidget(MaterialApp(home: GamePage(game: Game({}))));
 
     expect(find.byType(PlayerCard), findsWidgets);
   });
