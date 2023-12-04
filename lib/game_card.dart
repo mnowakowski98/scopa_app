33fe4c96505013ddf4c604181290976fbeaf8e63
Matthew Nowakowski <nowakowskim98@gmail.com>
2023-12-04 13:06:02 -0500

    Made dragging cards look less bad

--- lib/game_card.dart
+++ lib/game_card.dart
@@ -2,31 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:scopa_lib/tabletop_lib.dart' as tabletop_lib;
 
 class GameCard extends StatelessWidget {
-  const GameCard(
-      {super.key, required this.card, this.isSelected = false, this.onTap});
+  const GameCard({
+    super.key,
+    required this.card,
+    this.isSelected = false,
+    this.enableDragging = false,
+    this.onTap,
+  });
 
   final tabletop_lib.Card card;
   final bool isSelected;
+  final bool enableDragging;
   final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Draggable<tabletop_lib.Card>(
-      data: card,
-      feedback: const Placeholder(),
-      child: GestureDetector(
-        onTap: onTap,
-        child: Card(
-          color: isSelected ? Colors.blue : null,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              '${card.value}\n${card.suite}',
-              textAlign: TextAlign.center,
-            ),
-          ),
+    final body = Card(
+      color: isSelected ? Colors.blue : null,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          '${card.value}\n${card.suite}',
+          textAlign: TextAlign.center,
         ),
       ),
+    );
+
+    if (enableDragging == false) {
+      return GestureDetector(onTap: onTap, child: body);
+    }
+
+    return Draggable<tabletop_lib.Card>(
+      data: card,
+      feedback: body,
+      child: GestureDetector(onTap: onTap, child: body),
     );
   }
 }
