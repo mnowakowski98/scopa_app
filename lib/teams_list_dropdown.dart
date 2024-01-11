34182148a58749d14b78e02e9736fbb055b7c77f
Matthew Nowakowski <nowakowskim98@gmail.com>
2024-01-11 15:45:52 -0500

    Implemented showing team names on tap

--- lib/teams_list_dropdown.dart
+++ lib/teams_list_dropdown.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:scopa_lib/tabletop_lib.dart';
 
 class TeamsListDropdown extends StatelessWidget {
-  const TeamsListDropdown({super.key, required this.teamNames});
+  const TeamsListDropdown({super.key, required this.teams});
 
-  final List<String> teamNames;
+  final List<Team> teams;
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Implement
-    return const Placeholder();
+    return DropdownMenu(dropdownMenuEntries: [
+      for (final team in teams)
+        DropdownMenuEntry(value: team.name, label: team.name)
+    ]);
   }
 }
--- test/teams_list_dropdown_test.dart
+++ test/teams_list_dropdown_test.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:scopa_app/teams_list_dropdown.dart';
+import 'package:scopa_lib/tabletop_lib.dart';
 
 void main() {
-  testWidgets('displays a list of team names', (widgetTester) async {
-    const team1Name = 'Test player 1';
-    const team2Name = 'Test player 2';
+  testWidgets('displays a list of team names on tap', (widgetTester) async {
+    const team1Name = 'Test team 1';
+    const team2Name = 'Test team 2';
 
-    await widgetTester.pumpWidget(const MaterialApp(
-      home: TeamsListDropdown(
-        teamNames: [team1Name, team2Name],
+    await widgetTester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: TeamsListDropdown(
+          teams: [
+            Team.players([], name: '(Unassigned)'),
+            Team.players([], name: team1Name),
+            Team.players([], name: team2Name),
+          ],
+        ),
       ),
     ));
 
